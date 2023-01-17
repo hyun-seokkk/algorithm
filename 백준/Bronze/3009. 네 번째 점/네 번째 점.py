85c3b36f90a15fa85b98811b05b6151aa00baabc
hyundol2 <75524894+hyundol2@users.noreply.github.com>
2023-01-17 16:01:19 +0900

    [Bronze III] Title: 네 번째 점, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,14 @@
+import sys
+x_lst = []
+y_lst = []
+for _ in range(3):
+    x, y = map(int, sys.stdin.readline().split())
+    x_lst.append(x)
+    y_lst.append(y)
+
+for i in range(3):
+    if x_lst.count(x_lst[i]) == 1:
+        x = x_lst[i]
+    elif y_lst.count(y_lst[i]) == 1:
+        y = y_lst[i]
+print(x, y)
