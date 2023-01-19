8847420008d4548e5fd7150ce250ba63f1299728
hyundol2 <75524894+hyundol2@users.noreply.github.com>
2023-01-19 15:35:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 192 ms, Memory: 49632 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,15 +1,10 @@
 import sys
-from collections import Counter
 n = int(sys.stdin.readline())
-log = [sys.stdin.readline().strip().split() for _ in range(n)]
-""" empolyee = []
-for i in log:
-    empolyee.append(i[0]) """
-dic = {}
-for i in log:
-    dic[i[0]] = i[1]
-enter_dic = {}
-for k, v in dic.items():
-    if v == 'enter':
-        enter_dic[k] = [v]
-print(*sorted(enter_dic, reverse=True), sep='\n') 
+log = {}
+for i in range(n):
+    a, b = sys.stdin.readline().split()
+    if b == 'enter':
+        log[a] = b
+    else:
+        log.pop(a)
+print(*sorted(log, reverse=True), sep='\n')
