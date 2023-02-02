843af4e5fb51a9e06425a02bcc3b83b8f042ca97
hyundol2 <75524894+hyundol2@users.noreply.github.com>
2023-02-02 17:04:08 +0900

    [Silver V] Title: 단어 나누기, Time: 56 ms, Memory: 33300 KB -BaekjoonHub

---
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -0,0 +1,10 @@
+word = input()
+res = []
+
+for i in range(len(word)-2):
+    for j in range(i+1, len(word)-1):
+        for k in range(j+1, len(word)):
+            temp = word[:j][::-1] + word[j:k][::-1] + word[k:][::-1]
+            res.append(temp)
+
+print(min(res))
