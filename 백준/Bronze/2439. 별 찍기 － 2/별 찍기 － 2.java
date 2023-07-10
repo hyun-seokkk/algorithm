8bfdafc94768d6fd0378ab98e923eb0eb6a6c153
hyundol2 <75524894+hyundol2@users.noreply.github.com>
2023-07-10 22:43:04 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 360 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		for (int i=1; i<=x; i++) {
+			for (int j=1; j<=x-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+
+}
