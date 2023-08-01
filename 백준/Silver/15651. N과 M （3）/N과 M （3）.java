ec7ec25d11aa62182d0a255e5c4a7b61e3950b00
hyundol2 <75524894+hyundol2@users.noreply.github.com>
2023-08-02 00:01:58 +0900

    [Silver III] Title: N과 M (3), Time: 596 ms, Memory: 207320 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int N;
+	static int M;
+	static int[] sel;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		st = new StringTokenizer(br.readLine()," ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		sel = new int[M];
+		
+		recursive(0);
+		System.out.println(sb);
+	}
+	private static void recursive(int idx) {
+		if(idx == M) {
+			for(int ele : sel) {
+				sb.append(ele+" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=0; i<N; i++) {
+			sel[idx] = i+1;
+			recursive(idx+1);
+		}
+	}
+
+}
+
